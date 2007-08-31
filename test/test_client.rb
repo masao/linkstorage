0e43b2910203f5aa818dd49a618412099aaacc0e
masao <tmasao@acm.org>
2007-08-31 08:31:21 +0000

    change assert

--- test/test_client.rb
+++ test/test_client.rb
@@ -8,6 +8,7 @@ require "client.rb"
 
 class TC_CLIENT < Test::Unit::TestCase
    def test_initialize
-      assert( LinkStorage::Client.new( "http://localhost/~masao/private/cvswork/linkstorage/api.cgi/example" ) )
+      client = LinkStorage::Client.new( "http://localhost/~masao/private/cvswork/linkstorage/api.cgi/example" )
+      assert( client )
    end
 end
