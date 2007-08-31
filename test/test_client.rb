642f4926346998bb7fa6dc938975349ca30afc64
masao <tmasao@acm.org>
2007-08-31 09:12:24 +0000

    test_store

--- test/test_client.rb
+++ test/test_client.rb
@@ -6,9 +6,22 @@ require "ftools"
 
 require "client.rb"
 
+user, passwd = open(".localhost.passwd").read.chomp.split(/:/)
+
 class TC_CLIENT < Test::Unit::TestCase
+   def setup
+      File.rm_f "data/example.db"
+   end
+   
    def test_initialize
       client = LinkStorage::Client.new( "http://localhost/~masao/private/cvswork/linkstorage/api.cgi/example" )
       assert( client )
+   end
+   def test_store
+      client = LinkStorage::Client.new( "http://localhost/~masao/private/cvswork/linkstorage/api.cgi/example", user, passwd )
+      set = [ 1, 2, 3 ]
+      response = client.save( set )
+      puts response.body
+      assert_equal( "200", response.code, "HTTP status code" )
    end
 end
