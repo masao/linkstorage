be5b8bc355719ed1b75c2a39d986ce9c88475bb7
masao <tmasao@acm.org>
2007-09-11 07:47:57 +0000

    class HTTPConnection

--- client.rb
+++ client.rb
@@ -10,25 +10,43 @@ require "data.rb"
 module LinkStorage
    class Client
       def initialize( baseurl, user = nil, password = nil )
-         @baseurl = baseurl
-         @uri = URI.parse( @baseurl )
-         @user, @password = user, password
+         @http = HTTPConnection.new( baseurl, user, password )
       end
       def save( set, delegate = nil )
          delegate = set[0] if delegate.nil?
          data = [ set.map{|e| "set=#{e}" },
                   "delegate=#{delegate}" ].flatten.join("&")
-         http = Net::HTTP.new( @uri.host, @uri.port )
-         if not @user.nil? and not @password.nil?
-            auth = [ "#{@user}:#{@password}" ].pack('m').gsub(/\n/, '')
-            header = { "Authorization" => "Basic " + auth }
+         @http.request( :post, data )
+      end
+      def query( query )
+         @http.request( :get, "query=#{query}" )
+      end
+
+      class HTTPConnection
+         def initialize( baseurl, user = nil, password = nil )
+            @uri = URI.parse( baseurl )
+            @http = Net::HTTP.new( @uri.host, @uri.port )
+            @user, @password = user, password
          end
-         response = http.post( @uri.path, data, header )
-         #p response.body
-         unless response.code == "200"
-            raise "error"
+         def request( method, data = nil )
+            response = nil
+            if not @user.nil? and not @password.nil?
+               auth = [ "#{@user}:#{@password}" ].pack('m').gsub(/\n/, '')
+               header = { "Authorization" => "Basic " + auth }
+            end
+            case method
+            when :post
+               response = @http.post( @uri.path, data, header )
+            when :get
+               response = @http.get( @uri.path + "?" + data, header )
+            else
+               raise "unknown request method"
+            end
+            unless response.code == "200"
+               raise "error occured: #{response.code}: #{response.body}"
+            end
+            Data.load_xml( response.body )
          end
-         Data.load_xml( response.body )
       end
    end
 end
